2099cae36162cec983918dc4a73f35c5eb13bbff
Satish Iyer <satish.viswanathan.in@gmail.com>
2026-05-21 05:30:49 +0530

    Changes Made

--- js/toc.js
+++ js/toc.js
@@ -1,45 +1,62 @@
-var spy = function () {
-  var elems = document.querySelectorAll(Array.from(Array(6).keys(), x => ".post-body h"+(x+1).toString()));
-  // ":is()" was not supported until Chrome 88+
-  // Here is a backfill
-  if (elems.length == 0) {
-    return;
-  }
-  var supportPageOffset = window.pageXOffset !== undefined;
-  var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+"use strict";
 
-  var currentTop = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
-  var currentBottom = currentTop + window.height;
-  var pageBottom = window.pageBottom;
-
-  var meetUnread = false
-  let lastElemName = elems[elems.length - 1].id;
-  elems.forEach(function (elem, idx) {
-    var elemTop = elem.offsetTop;
-    var id = elem.getAttribute('id');
-    var navElems = document.getElementsByClassName("nav-"+id);
-    if (navElems.length == 0) {
-      return
-    }
-    if (currentTop >= elemTop || currentBottom >= pageBottom) {
-      Array.from(navElems).forEach((e) => {
-        e.classList.add('toc-active');
-      });
-    } else {
-      if (meetUnread == false) {
-        meetUnread = true;
-        if (idx > 0) {
-          lastElemName = elems[idx - 1].id; 
-        }
+function _toConsumableArray(r) { return _arrayWithoutHoles(r) || _iterableToArray(r) || _unsupportedIterableToArray(r) || _nonIterableSpread(); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _iterableToArray(r) { if ("undefined" != typeof Symbol && null != r[Symbol.iterator] || null != r["@@iterator"]) return Array.from(r); }
+function _arrayWithoutHoles(r) { if (Array.isArray(r)) return _arrayLikeToArray(r); }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function tocHighlighter() {
+  return {
+    headings: [],
+    links: [],
+    debouncedScroll: null,
+    init: function init() {
+      var toc = document.querySelector('#TableOfContents');
+      if (!toc || window.getComputedStyle(toc.parentElement).display === 'none') {
+        return;
       }
-      Array.from(navElems).forEach((e) => {
-        e.classList.remove('toc-active');
+      this.links = _toConsumableArray(toc.querySelectorAll('a'));
+      this.headings = this.links.map(function (link) {
+        var _link$getAttribute;
+        return document.getElementById((_link$getAttribute = link.getAttribute('href')) === null || _link$getAttribute === void 0 ? void 0 : _link$getAttribute.replace('#', ''));
+      }).filter(Boolean);
+      this.debouncedScroll = this.debounce(this.onScroll.bind(this), 300);
+      this.debouncedScroll();
+    },
+    onScroll: function onScroll() {
+      var closest = null;
+      var minOffset = Infinity;
+      this.headings.forEach(function (el, index) {
+        var rect = el.getBoundingClientRect();
+        var offset = Math.abs(rect.top);
+        if (rect.top < window.innerHeight && offset < minOffset) {
+          minOffset = offset;
+          closest = index;
+        }
       });
+      if (closest !== null && this.links[closest]) {
+        this.setActive(this.links[closest]);
+      }
+    },
+    setActive: function setActive(activeLink) {
+      this.links.forEach(function (link) {
+        return link.classList.remove('menu-active');
+      });
+      activeLink.classList.add('menu-active');
+    },
+    debounce: function debounce(fn, delay) {
+      var timeout;
+      return function () {
+        var _this = this;
+        for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+          args[_key] = arguments[_key];
+        }
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+          return fn.apply(_this, args);
+        }, delay);
+      };
     }
-  })
-  let selector = ".nav-" + lastElemName;
-  // Two toc elements here
-  document.querySelectorAll(selector).forEach(e => {
-    e.scrollIntoView({ block: "center", behavior: 'smooth' });
-  });
+  };
 }
